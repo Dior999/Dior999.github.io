7a0b8f39bccd2e23f460daabb3881a4e08c91433
Dior999 <38903169+Dior999@users.noreply.github.com>
2018-07-12 15:15:56 +0800

    Add files via upload

---
+++ lodash/dior999-lodash.js
@@ -0,0 +1,9 @@
+var Dior999 = {
+  chunk: function (array, size){
+    var myCars = []
+    for (var i = 0; i < array.length; i = i + size){
+      myCars.push(array.slice(i,i+size))
+    }
+    return myCars
+  }
+}
