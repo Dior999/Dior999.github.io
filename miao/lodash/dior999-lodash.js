d67897dbe902fae657675cbf41253e4fb95a7e13
Dior999 <38903169+Dior999@users.noreply.github.com>
2018-07-18 09:53:29 +0800

    Update dior999-lodash.js

--- miao/lodash/dior999-lodash.js
+++ miao/lodash/dior999-lodash.js
@@ -9,7 +9,7 @@ var dior999 = {
   compact: function (array) {
     var ary = []
     for (var i = 0; i < array.length; i++){
-      if (array[i] != false && array[i]!=NaN && array[i] != "" && array[i] != undefined && array[i]!= 0 &&array[i]!=null){
+      if (array[i] != false && array[i]!=NaN && array[i] != "" && array[i] != undefined && array[i]!= 0 &&array[i]!=null && array[i]!="null"){
         ary.push(array[i])
       }
     }
